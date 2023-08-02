8c638f540f39f8e7dafaa558b6564f776218d145
mariaeduardx <123771597+mariaeduardx@users.noreply.github.com>
2023-08-02 13:22:30 -0300

    Add files via upload

---
+++ dog.js
@@ -0,0 +1,26 @@
+// 1
+const myAge = 30; // Armazena a sua idade em anos  humanos
+
+// 2
+let earlyYears = 2; // Os dois primeiros anos de um cão, que contam mais.
+
+// 3
+earlyYears *= 10.5; // Multiplica earlyYears por 10.5 para obter "anos de cão".
+
+// 4
+let laterYears = myAge - 2; // Subtrai 2 da sua idade para calcular os anos subsequentes.
+
+// 5
+laterYears *= 4; // Multiplica laterYears por 4 para obter "anos de cão".
+
+// 6
+console.log(earlyYears, laterYears);
+
+// 7
+let myAgeInDogYears = earlyYears + laterYears; // Calcula a sua idade em "anos de cão".
+
+// 8
+let myName = "Seu Nome".toLowerCase(); // Converte seu nome para letras minúsculas.
+
+// 9
+console.log(`Meu nome é ${maria}. Tenho ${17} anos em anos humanos, que são ${119} anos em anos de cão.`);
